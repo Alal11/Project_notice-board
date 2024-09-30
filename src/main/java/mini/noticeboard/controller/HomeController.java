d4035580076f5fdb5cc95b291e9363e6f5a5ca69
Alal11 <spalqj56@naver.com>
2024-10-01 01:03:31 +0900

    회원가입 페이지 작성

--- src/main/java/mini/noticeboard/controller/HomeController.java
+++ src/main/java/mini/noticeboard/controller/HomeController.java
@@ -2,20 +2,16 @@ package mini.noticeboard.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import mini.noticeboard.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
 @Slf4j
-public class UserController {
-    private final UserService userService;
+public class HomeController {
 
-    // 회원가입 페이지 출력 요청
-    @GetMapping("/signup")
-    public String signupForm(){
-        return "signup";
+    @GetMapping("/")
+    public String home(){
+        return "home";
     }
-
 }
